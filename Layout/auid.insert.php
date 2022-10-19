4374a9295a612937ed3cba2bb396020d7315bfc4
Tummemo <113905858+Tummemo@users.noreply.github.com>
2022-10-19 18:05:53 +0630

    Create auid.insert.php

---
+++ Layout/auid.insert.php
@@ -0,0 +1,11 @@
+<?php
+include("json_table.class.php"); ?>
+<html><head><title>AUID INSERT TABLE </title><link rel="stylesheet" href="CSS/common.css"> </head><body>   
+<div class="container">
+<center> 
+<?php Auid::if_insert_isset();
+?>
+<center>
+</div>
+</body>
+</html>
