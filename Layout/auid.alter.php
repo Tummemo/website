7e7a97c36fa812e18bcc4ab06a0f6c98b7c27cd9
Tummemo <113905858+Tummemo@users.noreply.github.com>
2022-10-19 18:06:47 +0630

    Create auid.alter.php

---
+++ Layout/auid.alter.php
@@ -0,0 +1,11 @@
+<?php
+include("json_table.class.php"); ?>
+<html><head><title>AUID ALTER TABLE </title><link rel="stylesheet" href="CSS/common.css"> </head><body>   
+<div class="container">
+<center> 
+<?php Auid::if_alter_isset();
+?>
+<center>
+</div>
+</body>
+</html>
