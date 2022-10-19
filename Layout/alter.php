9557fe95551e0f92fb9da16e9d51ce21e08c9acc
Tummemo <113905858+Tummemo@users.noreply.github.com>
2022-10-19 18:04:58 +0630

    Create alter.php

---
+++ Layout/alter.php
@@ -0,0 +1,13 @@
+<?php
+session_start(); 
+include("json_table.class.php"); ?>
+<html><head><title>Alter Table</title><link rel="stylesheet" href="CSS/common.css"> </head><body>   
+<div class="container">
+<center> 
+
+
+<center>
+</div>
+</body>
+</html>
+?>
