fd956b6d8d8725a04be7652a5274e6dd24c68161
Tummemo <113905858+Tummemo@users.noreply.github.com>
2022-10-20 09:45:43 +0630

    Update student.class.php

--- Layout/student.class.php
+++ Layout/student.class.php
@@ -1,11 +1,22 @@
 <?php
 include("index.class.php");
+include("student.page.class.php");
 class Student{
         public static $value = 1;
         public static $val = 0;
         public static function page_header($title){
 session_start();   ?>      
-<html><head><title><?php echo $title; ?></title><link rel="stylesheet" href="CSS/common.css"> </head><body>   
+<html><head>
+<meta charset="UTF-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+<title><?php echo $title; ?>
+</title>
+
+<link rel="stylesheet" href="CSS/common.css"> 
+<style>
+
+
+</style>
+</head><body>   
 
 <?php     
 
@@ -39,7 +50,10 @@ session_start();   ?>
        
        if(self::$val > 0 ){
                   
-              //PAGE SHOW 
+              /* page show */ 
+              
+              Studentpage::page();
+              Studentpage::opt();
               
               }//if_user_valid 
               else{
